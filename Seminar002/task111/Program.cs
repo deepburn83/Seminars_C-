549e4b3d234cab5088d19a9bb6a49653470d2a17
Victor Gudiloff <deepburn1983@gmail.com>
2023-08-23 21:47:03 +0300

    написал программу подсчета часов и минут

---
+++ Seminar002/task111/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, подсчитывающая количество часов и минут.
+
+int InputMinutes(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int timeInMinutes = InputMinutes("Введите любое количество минут: ");
+int hour = timeInMinutes / 60;
+int minute = timeInMinutes % 60;
+System.Console.Write($"{hour} часов, {minute} минут");
