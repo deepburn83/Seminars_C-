56be59527c2d7c38feb8d64ef56f2329d97faa18
Victor Gudiloff <deepburn1983@gmail.com>
2023-09-11 00:02:25 +0300

    Выполнено домашнее задание за 5-й семинар (№№ 34, 36, 38)

---
+++ Seminar006/task039/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 39:
+// Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int InputNum (string message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+return new int[size];
+}
+
+void FillArray (int[] arr, int minValue, int maxValue)
+{
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+arr[i] = rnd.Next(minValue, maxValue + 1);
+}
+
+void PrintArray(int[] arr)
+{
+for (int i = 0; i < arr.Length; i++)
+Console.Write(arr[i] + " ");
+Console.WriteLine();
+}
+
+void ReverseArray(int[] arr)
+{
+for (int i = 0; i < arr.Length / 2; i++)
+{
+int temp = arr[i];
+arr[i] = arr[arr.Length - 1 - i];
+arr[arr.Length - 1 - i] = temp;
+// (arr[i], arr[arr.Length - 1 - i]) = (arr[arr.Length - 1 - i], arr[i]);
+}
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] array = CreateArray(size);
+
+int min = InputNum("Введите минимальное значение элемента: ");
+int max = InputNum("Введите максимальное значение элемента: ");
+
+FillArray(array, min, max);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
---
+++ Seminar006/task040/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40:
+// Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+
+// Теорема о неравенстве треугольника:
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+bool TriangleLength(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+int lenth1 = InputNum("Введите длину первой стороны треугольника: ");
+int lenth2 = InputNum("Введите длину второй стороны треугольника: ");
+int lenth3 = InputNum("Введите длину третьей стороны треугольника: ");
+
+bool answer = TriangleLength(lenth1, lenth2, lenth3);
+Console.WriteLine(answer? "Да" : "Нет"); // answer == true?
