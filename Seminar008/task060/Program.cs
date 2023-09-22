171dca2218962716583f1315536761e1dc34c5c5
Victor Gudiloff <deepburn1983@gmail.com>
2023-09-22 15:06:38 +0300

    внесены изменения в задачу 60

--- Seminar008/task060/Program.cs
+++ Seminar008/task060/Program.cs
@@ -8,6 +8,9 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+const int min = 10;
+const int max = 99;
+
 int InputNum(string message)
 {
     Console.Write(message);
@@ -23,16 +26,16 @@ int[] CreatRandomArray(int size)
 {
     int[] array = new int[size];
     Random rnd = new Random();
-    array[0] = rnd.Next(10, 100);
+    array[0] = rnd.Next(min, max + 1);
 
     for (int i = 1; i < array.Length; i++)
     {
-        array[i] = rnd.Next(10, 100);
+        array[i] = rnd.Next(min, max + 1);
         for (int j = 0; j < i; j++)
         {
             while (array[i] == array[j])
             {
-                array[i] = rnd.Next(10, 100);
+                array[i] = rnd.Next(min, max + 1);
                 j = 0;
             }
         }
@@ -56,19 +59,12 @@ void Fill3DArray(int[,,] array, int[] randomArray)
 void Print3DArray(int[,,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
-    {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
                 Console.Write($"{array[i, j, k]} ({i}, {j}, {k})\t");
             Console.WriteLine();
         }
-    }
-}
-
-int SizeArray(int rows, int columns, int layers)
-{
-    return rows * columns * layers;
 }
 
 Console.Clear();
@@ -77,15 +73,14 @@ int myRows = InputNum("Введите количество строк трехм
 int myColumns = InputNum("Введите количество столбцов трехмерной матрицы: ");
 int myLayers = InputNum("Введите количество слоёв трехмерной матрицы: ");
 
-if (SizeArray(myRows, myColumns, myLayers) <= 90)
+int[,,] resultArray = Create3DArray(myRows, myColumns, myLayers);
+if (resultArray.Length <= max - min + 1)
 {
-    int amountArray = SizeArray(myRows, myColumns, myLayers);
-    int[,,] myArray = Create3DArray(myRows, myColumns, myLayers);
-    int[] randomArray = CreatRandomArray(amountArray);
+    int[] randomArray = CreatRandomArray(resultArray.Length);
     Console.WriteLine();
     Console.WriteLine($"Трехмерный массив размером {myRows}x{myColumns}x{myLayers}:");
-    Fill3DArray(myArray, randomArray);
-    Print3DArray(myArray);
+    Fill3DArray(resultArray, randomArray);
+    Print3DArray(resultArray);
 }
 else
-    Console.Write("Неповторяющихся двухзначных чисел меньше, чем размерность массива!");
+    Console.Write("Размер массива превышает возможное количество неповторяющихся двузначих чисел!");
