7eb25ef95b04660d190c9f3ca2d31634a64c3ab8
Victor Gudiloff <deepburn1983@gmail.com>
2023-09-22 21:41:18 +0300

    Добавлено домашнее задание по 9-му семинару

---
+++ Seminar009/task066/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66.
+// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SumNum(int M, int N)
+{
+    if (N < M) return 0;
+    else if (M == N) return N;
+    else return N + SumNum(M, --N);
+}
+
+Console.Clear();
+
+int first = InputNum("Введите число M:");
+int last = InputNum("Введите число N:");
+int sum = SumNum(first, last);
+Console.WriteLine(sum);
---
+++ Seminar009/task068/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 68.
+// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Ack(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Ack(m - 1, 1);
+    else
+        return Ack(m - 1, Ack(m, n - 1));
+}
+
+Console.Clear();
+
+int first = InputNum("Введите число M:");
+int second = InputNum("Введите число N:");
+int result = Ack(first, second);
+System.Console.WriteLine($"A(m,n) = {result}");
